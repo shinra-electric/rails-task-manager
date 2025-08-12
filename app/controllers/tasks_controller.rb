3e0d4b21b86b9e6da78189a4a705f5a73dbb5041
Seamus O'Connor <shinra.electric@icloud.com>
2025-08-12 15:01:19 +0900

    Initial commit

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+
+    if @task.completed 
+      @message = "This task is completed!"
+    else 
+      @message = "This task hasn't been completed yet"
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task)
+    else 
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
+   private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/tasks', to: 'tasks#index', as: :tasks
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update', as: :task
+
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  get '/tasks/:id', to: 'tasks#show'
 end
